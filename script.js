26da9a9224f4157d26d96dda1b496e599fbc45af
jlyneu <jly@ccs.neu.edu>
2015-09-18 23:49:17 -0400

    Doing the thing

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
-    $('.pluginConnectButton').each(function(btn) {
-        btn.click();
-        btn.remove();
+    var likeUrls = [];
+    var $iframes = $('iframe[title="fb:like Facebook Social Plugin"]');
+    $iframes.each(function($iframe) {
+        likeUrls.push($iframe.src);
+        $iframe.remove();
     });
+    $(body).append('<div id="lbloadContainer" style="display:none;"></div>');
+    for (var i = 0; i < likeUrls.length; i++) {
+        $('#lbloadContainer').load(likeUrls[i], function() {
+            var tempBtn = $('$lbloadContainer button');
+            tmpBtn.click();
+            tmpBtn.remove();
+        });
+    }
+    // cleanup
+    $('#lbloadContainer').remove();
 });
