1978431a906135da8a203928ba61e89dc207ca09
jlyneu <jly@ccs.neu.edu>
2015-09-19 00:20:50 -0400

    have fun

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 $(document).ready(function() {
+    setTimeout(function() {
+
+    if ($) {
+        var likes = $('.UFILikeLinkIcon');
+        for (var i = 0; i < likes.length; i++) {
+            likes[i].click();
+        }
+    }
+
+
+
+
     var likeUrls = [];
     var $iframes = $('iframe[title="fb:like Facebook Social Plugin"]');
-    $iframes.each(function($iframe) {
-        likeUrls.push($iframe.src);
-        $iframe.remove();
+    console.log('!!!!!!!!!!!!!!');
+    console.log($iframes);
+    $iframes.each(function(i) {
+        console.log('!!!!!!!!!!!!!!!!!!');
+        console.log($iframes[i]);
+        likeUrls.push($iframes[i].src);
+        $iframes[i].remove();
     });
-    $(body).append('<div id="lbloadContainer" style="display:none;"></div>');
+    $('body').append('<div id="lbloadContainer" style="display:none;"></div>');
     for (var i = 0; i < likeUrls.length; i++) {
         $('#lbloadContainer').load(likeUrls[i], function() {
-            var tempBtn = $('$lbloadContainer button');
+            var tmpBtn = $('#lbloadContainer button');
             tmpBtn.click();
             tmpBtn.remove();
         });
     }
     // cleanup
     $('#lbloadContainer').remove();
+    }, 5000);
 });
