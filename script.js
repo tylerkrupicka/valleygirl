14c24ef4bb98268b6ac442dbc9b0d62b9e0b96e1
Tyler Krupicka <tylerkrupicka@gmail.com>
2015-09-20 16:01:33 -0700

    added cookie checking

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
 
+    //like everything on the news feed
     var likes = $('.UFILikeLink');
-        for (var i = 0; i < likes.length; i++) {
-            likes[i].click();
-        }
+    for (var i = 0; i < likes.length; i++) {
+        likes[i].click();
     }
 
-    var likebtns = $('.pluginButton[title="Like"] button[title="Like"]');
-    $('div[title="Unlike"]').remove();
-    $('.pluginCountButton').remove();
-    for (var i = 0; i < likebtns.length; i++) {
-        var cNames = $('div[title="Like"]').attr('class');
-        var likebtn = likebtns[i];
-        if (!$('div[title="Like"]').hasClass('hidden_elem')) {
-            likebtn.click();
+    if(document.cookie.indexOf('c_user') != -1){ //chech if logged in before clicking
+        var likebtns = $('.pluginButton[title="Like"] button[title="Like"]'); //find all like buttons
+        $('div[title="Unlike"]').remove(); //remove unlike transition
+        $('.pluginCountButton').remove(); //remove like bubble
+        for (var i = 0; i < likebtns.length; i++) {
+            var cNames = $('div[title="Like"]').attr('class');
+            var likebtn = likebtns[i];
+            if (!$('div[title="Like"]').hasClass('hidden_elem')) {
+                likebtn.click(); //click button
+            }
         }
     }
 });
