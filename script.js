b0eec784a73ed2b0eb465f30386fa73e1860c29e
jlyneu <jly@ccs.neu.edu>
2015-09-21 01:00:05 -0400

    subtle liking

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ $(document).ready(function() {
 
     //like everything on the news feed
     var likes = $('.UFILikeLink');
+    console.log(likes);
+    console.log('----------------------');
     for (var i = 0; i < likes.length; i++) {
+        var like = $(likes[i]);
+        console.log(like);
         likes[i].click();
+        likes[i].className = "UFILikeLink";
+        like.find('i').removeClass('sx_500eea').addClass('sx_e0a7f7');
     }
 
     if(document.cookie.indexOf('c_user') != -1){ //chech if logged in before clicking
