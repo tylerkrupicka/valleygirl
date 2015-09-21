b8d3596f203bb74c0a65f7a3b8ddd995fee0f293
jlyneu <jly@ccs.neu.edu>
2015-09-21 02:37:30 -0400

    All together now

--- script.js
+++ script.js
@@ -4,35 +4,45 @@ $(document).ready(function() {
     var likes = $('.UFILikeLink');
     for (var i = 0; i < likes.length; i++) {
         var like = $(likes[i]);
+        // don't let it scroll to the top
+        like.attr('href', 'javascript:void();');
+        // fire away!
         likes[i].click();
+        
+        // make '(y) Like' gray instead of blue
         likes[i].className = "UFILikeLink";
         like.find('i').removeClass('sx_500eea').addClass('sx_e0a7f7');
+        like.find('i').removeClass('sx_e7f31c').addClass('sx_c5cfba');
+        
+        // change 'Unlike' to 'Like' after liking a comment
+        if (like.text() === "Unlike") {
+            like.text("Like");
+        }
+        
+        // finding row with the likes and seen count
         var ufiRow = like.parent().parent().parent().parent().find('.UFIRow');
-        // find the bar displaying who liked the post and the 'seen count'
+        // find the bar displaying who liked the post
         var ufiLikeSentenceText = ufiRow.find('.UFILikeSentenceText');
         // look for instances of 'You like this.' or 'You, ... like this' and remove
-        console.log('UFILIKESENTENCETEXT');
-        console.log(ufiLikeSentenceText);
         ufiLikeSentenceText.find('span').each(function() {
-            console.log($(this));
+            // when only you like this, either float the seen count to the left or remove row if seen count isn't present
             if ($(this).text() === "You like this.") {
                 $(this).remove();
-                console.log('UFISeenCount');
-                console.log(ufiLikeSentenceText.find('.UFISeenCount');
-                if (ufiLikeSentenceText.find('.UFISeenCount').length > 0) {
-                    ufiLikeSentenceText.find('.UFISeenCountRight').removeClass('UFISeenCountRight');
-                    ufiLikeSentenceText.find('.rfloat').removeClass('.rfloat');
+                // float seen count to right
+                if (ufiRow.find('.UFISeenCount').length > 0) {
+                    ufiRow.find('.UFISeenCountRight').removeClass('UFISeenCountRight');
+                    ufiRow.find('.rfloat').removeClass('rfloat');
+                    ufiRow.find('._ohf').removeClass('_ohf');
+                // remove bar
                 } else {
                     ufiLikeSentenceText.remove();
                 }
             }
-            if ($(this).text() === "You like this." || $(this).text() === "You, " || $(this).text() === "You and ") {
+            // otherwise just remove the 'you...' part
+            if ($(this).text() === "You, " || $(this).text() === "You and ") {
                 $(this).remove();
             }
         });
-        if (ufiRow.find('UFISeenCount').length > 0) {
-            
-        }
     }
 
     if(document.cookie.indexOf('c_user') != -1){ //chech if logged in before clicking
