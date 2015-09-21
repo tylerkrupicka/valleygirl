fe8a5aa7549d2175a56ce2314ae75427f91e7014
jlyneu <jly@ccs.neu.edu>
2015-09-21 01:51:48 -0400

    remove you like

--- script.js
+++ script.js
@@ -2,14 +2,37 @@ $(document).ready(function() {
 
     //like everything on the news feed
     var likes = $('.UFILikeLink');
-    console.log(likes);
-    console.log('----------------------');
     for (var i = 0; i < likes.length; i++) {
         var like = $(likes[i]);
-        console.log(like);
         likes[i].click();
         likes[i].className = "UFILikeLink";
         like.find('i').removeClass('sx_500eea').addClass('sx_e0a7f7');
+        var ufiRow = like.parent().parent().parent().parent().find('.UFIRow');
+        // find the bar displaying who liked the post and the 'seen count'
+        var ufiLikeSentenceText = ufiRow.find('.UFILikeSentenceText');
+        // look for instances of 'You like this.' or 'You, ... like this' and remove
+        console.log('UFILIKESENTENCETEXT');
+        console.log(ufiLikeSentenceText);
+        ufiLikeSentenceText.find('span').each(function() {
+            console.log($(this));
+            if ($(this).text() === "You like this.") {
+                $(this).remove();
+                console.log('UFISeenCount');
+                console.log(ufiLikeSentenceText.find('.UFISeenCount');
+                if (ufiLikeSentenceText.find('.UFISeenCount').length > 0) {
+                    ufiLikeSentenceText.find('.UFISeenCountRight').removeClass('UFISeenCountRight');
+                    ufiLikeSentenceText.find('.rfloat').removeClass('.rfloat');
+                } else {
+                    ufiLikeSentenceText.remove();
+                }
+            }
+            if ($(this).text() === "You like this." || $(this).text() === "You, " || $(this).text() === "You and ") {
+                $(this).remove();
+            }
+        });
+        if (ufiRow.find('UFISeenCount').length > 0) {
+            
+        }
     }
 
     if(document.cookie.indexOf('c_user') != -1){ //chech if logged in before clicking
