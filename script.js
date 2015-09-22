0457af82ef67261454bb19822ba19e0b9fa2c8d9
Tyler Krupicka <tylerkrupicka@gmail.com>
2015-09-21 18:15:45 -0700

    added functions, fixed "you" hide error

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-$(document).ready(function() {
+//click every like button on facebook
+function likeAllFacebook(callback){
+      //like everything on the news feed
+      var likes = $('.UFILikeLink');
+      for (var i = 0; i < likes.length; i++) {
+          var like = $(likes[i]);
+          // don't let it scroll to the top
+          like.attr('href', 'javascript:void();');
+          // fire away!
+          likes[i].click();
+      }
+      callback();
+}
 
-    //like everything on the news feed
+//hide the likes and make it seem normal
+function cleanupFacebook(){
     var likes = $('.UFILikeLink');
     for (var i = 0; i < likes.length; i++) {
         var like = $(likes[i]);
-        // don't let it scroll to the top
-        like.attr('href', 'javascript:void();');
-        // fire away!
-        likes[i].click();
-        
+
         // make '(y) Like' gray instead of blue
         likes[i].className = "UFILikeLink";
         like.find('i').removeClass('sx_500eea').addClass('sx_e0a7f7');
         like.find('i').removeClass('sx_e7f31c').addClass('sx_c5cfba');
-        
+
         // change 'Unlike' to 'Like' after liking a comment
         if (like.text() === "Unlike") {
             like.text("Like");
         }
-        
+
         // finding row with the likes and seen count
         var ufiRow = like.parent().parent().parent().parent().find('.UFIRow');
         // find the bar displaying who liked the post
@@ -27,7 +36,7 @@ $(document).ready(function() {
         ufiLikeSentenceText.find('span').each(function() {
             // when only you like this, either float the seen count to the left or remove row if seen count isn't present
             if ($(this).text() === "You like this.") {
-                $(this).remove();
+                $(this).hide();
                 // float seen count to right
                 if (ufiRow.find('.UFISeenCount').length > 0) {
                     ufiRow.find('.UFISeenCountRight').removeClass('UFISeenCountRight');
@@ -40,22 +49,35 @@ $(document).ready(function() {
             }
             // otherwise just remove the 'you...' part
             if ($(this).text() === "You, " || $(this).text() === "You and ") {
-                $(this).remove();
+                $(this).hide();
             }
         });
     }
+}
 
-    if(document.cookie.indexOf('c_user') != -1){ //chech if logged in before clicking
-        var likebtns = $('.pluginButton[title="Like"] button[title="Like"]'); //find all like buttons
-        $('div[title="Unlike"]').remove(); //remove unlike transition
-        $('.pluginCountButton').remove(); //remove like bubble
-        $('._5jjm').parent().remove();
-        for (var i = 0; i < likebtns.length; i++) {
-            var cNames = $('div[title="Like"]').attr('class');
-            var likebtn = likebtns[i];
-            if (!$('div[title="Like"]').hasClass('hidden_elem')) {
-                likebtn.click();
-            }
+//click like buttons on sites external to Facebook
+function clickiFrameButtons(){
+    var likebtns = $('.pluginButton[title="Like"] button[title="Like"]'); //find all like buttons
+    $('div[title="Unlike"]').remove(); //remove unlike transition
+    $('.pluginCountButton').remove(); //remove like bubble
+    $('._5jjm').parent().remove();
+    for (var i = 0; i < likebtns.length; i++) {
+        var cNames = $('div[title="Like"]').attr('class');
+        var likebtn = likebtns[i];
+        if (!$('div[title="Like"]').hasClass('hidden_elem')) {
+            likebtn.click();
         }
+    }
+}
+
+$(document).ready(function() {
+    if(document.cookie.indexOf('c_user') != -1){ //check if logged in before running anything
+
+        likeAllFacebook(function(){
+            cleanupFacebook();
+        });
+
+        clickiFrameButtons();
+
     }
 });
